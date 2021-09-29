d89020e94752a0d1750d0556b8cceb1fcdf9dffe
Michael Squire Obeng <michaelsquireobeng@gmail.com>
2021-09-29 22:29:58 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table cellSpacing="0" cellPadding="0" width="100%" border="0">
+                <tbody>
+                <tr>
+                    <td> 
+                        {/* // container start */}
+                        <table className="container" width="600px" align="center" cellPadding="0" cellSpacing="0" border="0">
+                            <tbody>
+                            {/* // Row start */}
+                            <tr>
+                                <td>
+                                  {/* // Left header Column */}
+                                  <table className="header-left" width="50%" align="left" cellPadding="0" cellSpacing="0" border="0" style={{background: '#65fffd', background: '-moz-linear-gradient(-45deg, #65fffd 0%, #2296ee 100%)', background: '-webkit-linear-gradient(-45deg, #65fffd 0%,#2296ee 100%)', background: 'linear-gradient(135deg, #65fffd 0%,#2296ee 100%)', fontFamily: 'Open Sans', color: '#ffffff', padding: '15px 15px 65px 15px'}}>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <img src="/img/logo.png" alt="logo" />
+                                                <h2 style={{fontSize: '28px', fontWeight: '400', margin: '0', lineHeight: '1', padding: '40px 0px 10px'}}>Get Ready For</h2>
+                                                <h1 style={{fontSize: '80px', margin: '0', lineHeight: '1',fontWeight: '700'}}>HERO</h1>
+                                                <p style={{fontSize: '14px', fontWeight: '400', margin: '0', lineHeight: '1.5',fontStyle: 'italic', padding: '30px 0px 40px'}}>
+                                                I'm baby roof party irony cronut blue bottle. Cred taiyaki helvetica, hexagon portland readymade snackwave man braid yr.
+                                                </p>
+                                                <a href="" style={{textDecoration: 'none', color: '#ffffff', fontSize: '12px', border: '2px solid #ffffff', borderRadius: '3px', padding: '10px 15px', textTransform: 'uppercase'}}>Learn More</a>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    {/* // Left header Column End*/}
+                                    {/* // Right header Column */}
+                                    <table className="header-right" width="50%" align="right" cellPadding="0" cellSpacing="0" border="0" >
+                                        <tbody>
+                                        <tr>
+                                            <td style={{fontFamily: 'Open Sans', color: '#7d7d7d'}}>
+                                            <table className="container" width="100%" align="center" cellPadding="0" cellSpacing="0" border="0" style={{padding: '20px'}}>
+                                              <tbody>
+                                                <tr>
+                                                  <td align="center">
+                                                    <a style={{fontSize: '13px', fontWeight: '700', textDecoration: 'none', textTransform: 'uppercase', margin: '0 auto'}}>About</a>
+                                                  </td>
+                                                  <td align="center">
+                                                    <a style={{fontSize: '13px', fontWeight: '700', textDecoration: 'none', textTransform: 'uppercase', margin: '0 auto'}}>Careers</a>
+                                                  </td>
+                                                  <td align="center">
+                                                    <a style={{fontSize: '13px', fontWeight: '700', textDecoration: 'none', textTransform: 'uppercase', margin: '0 auto'}}>Contact</a>
+                                                  </td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                          <td align="center" style={{paddingTop: '36px'}}><img src="/img/smartwatch.png"/></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    {/* // Right header Column End*/}
+                                </td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            {/* // Row end */}
+                            {/* // Row start */}
+                            <tr>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                            {/* // Row end */}
+                        </table>
+                        {/* // container start */}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
   );
 }
 
